e40248e96cda9efb2f2af8d8e6617f55683428c6
Aaron <AaronTJDev@gmail.com>
2021-04-26 15:05:24 -0700

    fixed dotenv config location

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./data/schema');
 
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, '/dist')));
 
