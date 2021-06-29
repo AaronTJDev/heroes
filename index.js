5da19aa4745fde95d3ff2d44c0eeffe791e6cf40
Aaron <AaronTJDev@gmail.com>
2021-06-28 22:31:44 -0700

    Updated ips

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use('/api', graphqlHTTP({
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 });
+
 app.listen(port, () => {
     console.log(`Server running on ${port}`);
 })
